cd8e84d2d38d2db994d5fb440bd7c4663dbe825e
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-03-01 11:41:02 -0500

    Adding fibonacci recurse version to see how spans look like.

--- src/add.rs
+++ src/add.rs
@@ -1,8 +1,28 @@
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 pub fn add(f: u32, s: u32) -> u32 {
 	let span = tracing::info_span!("Add function", ?f, ?s);
 	let _guard = span.enter();
 	tracing::info!("Info event");
 	f.add(s)
+}
+
+pub fn fibonacci(till: u32) -> u32 {
+	fib(till - 1) + fib(till - 2)
+}
+
+pub fn fib(till: u32) -> u32 {
+	if till == 1 {
+		tracing::info!("base case 1");
+		1
+	} else if till == 0 {
+		tracing::info!("base case 0");
+		0
+	}
+	else {
+		let span = tracing::info_span!("Calculating for ", ?till);
+		let _guard = span.enter();
+
+		fib(till - 1) + fib(till - 2)
+	}
 }
